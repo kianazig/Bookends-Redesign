a8b683c329644c44ed68e18793e608a3be5731e2
Kevin Lu <klu91748@gmail.com>
2020-04-24 18:00:57 -0700

    fixed padding

--- src/ContactUs.java
+++ src/ContactUs.java
@@ -1,15 +1,12 @@
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
-
 
 public class ContactUs {
     
@@ -23,7 +20,7 @@ public class ContactUs {
     	Label contactLabel = new Label("Contact us!");
 		Label addressLabel = new Label("211 East Ridgewood Ave.  Ridgewood, NJ  07450");
 		Label questionLabel = new Label("Please write to us your questions.");
-		Label socialLabel = new Label("Follow on our socials!");
+		Label socialLabel = new Label("Follow us on our socials!");
 		
 		TextField text = new TextField("Enter your e-mail address.");
 		text.setOnMouseClicked(e -> {
@@ -48,7 +45,7 @@ public class ContactUs {
 		submit.setId("submit");
 		twitter.setId("twitter");
 		facebook.setId("facebook");
-		twitter.setPadding(new Insets(0, 0, 0, -10));
+		twitter.setPadding(new Insets(0, 0, 0, 50));
 		facebook.setPadding(new Insets(0, 0, -1, -10));
 		
 		GridPane gridpane = new GridPane();
@@ -59,12 +56,12 @@ public class ContactUs {
 		gridpane.add(submit, 0, 5);
 		gridpane.add(socialLabel, 0, 6);
 		gridpane.add(twitter, 0, 7);
-		gridpane.add(facebook, 0, 8);
+		gridpane.add(facebook, 0, 7);
 		gridpane.add(addressLabel, 1, 3);
 		gridpane.add(image, 1, 4);
 		gridpane.setHgap(100);
 		gridpane.setVgap(10);
-		gridpane.setPadding(new Insets(0, 0, 0 , 20));
+		gridpane.setPadding(new Insets(20, 0, 0, 100));
 		
 		StackPane stackpane = new StackPane();
 		stackpane.getChildren().add(gridpane);
@@ -92,6 +89,7 @@ public class ContactUs {
 		facebook.setOnMouseExited(e -> {
 			stackpane.setCursor(Cursor.DEFAULT);
 		});
+		
 		return stackpane;
     }
     
