a3d8d243a37cc834dfb29bc6fdf7744adcbd1532
kianazig <33229082+kianazig@users.noreply.github.com>
2020-04-26 02:19:15 -0700

    Events page completed.

--- src/Events.java
+++ src/Events.java
@@ -6,8 +6,11 @@ import java.util.List;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -22,14 +25,77 @@ public class Events {
     private List<Event> eventsList;
     private int selectedEvent;
     private List<StackPane> eventPreviews;
+    BorderPane eventFeature;
     
     public Events(){
     	selectedEvent = 0;
+    	eventFeature = new BorderPane();
     	initializeEventsList();
+    	updateEventFeature(eventsList.get(0));
     	createEventsPage();
     }
     
-    private void initializeEventsList() {
+    private void updateEventFeature(Event event) {
+    	eventFeature.getChildren().clear();
+    	
+    	Text eventFeatureTitle = new Text(event.title);
+    	eventFeatureTitle.setId("event-feature-title");
+    	eventFeature.setTop(eventFeatureTitle);
+    	
+    	
+		ImageView image = new ImageView(event.image);
+		image.setPreserveRatio(true);
+		image.setFitWidth(300);
+		
+		Text author = new Text("Author: "+event.author);
+		author.getStyleClass().add("event-feature-detail");
+		Text book = new Text("Book: "+event.book);
+		book.getStyleClass().add("event-feature-detail");
+		Text date = new Text("Date: "+event.date);
+		date.getStyleClass().add("event-feature-detail");
+		Text time = new Text("Time: "+event.time);
+		time.getStyleClass().add("event-feature-detail");
+		Text location = new Text("Location: "+event.location);
+		location.getStyleClass().add("event-feature-detail");
+		VBox eventDetails = new VBox(10, author, book, date, time, location);
+		
+		Text description = new Text(event.description);
+		description.getStyleClass().add("event-feature-description");
+		description.setWrappingWidth(290);
+		
+		VBox eventInformation = new VBox(30, eventDetails, description);
+		eventInformation.setAlignment(Pos.CENTER_LEFT);
+		
+		eventInformation.setMinWidth(300);
+		eventInformation.setMaxWidth(300);
+		
+		
+		Text buyTicketText = new Text("Buy Tickets!");
+		buyTicketText.setId("buy-tickets-text");
+		Image ticketImage = new Image("ticket-icon.png");
+		ImageView ticketIcon = new ImageView(ticketImage);
+		ticketIcon.setPreserveRatio(true);
+		ticketIcon.setFitHeight(40);
+		HBox buyTickets = new HBox(10, ticketIcon, buyTicketText);
+		buyTickets.setId("buy-tickets-box");
+		buyTickets.setAlignment(Pos.CENTER);
+		buyTickets.setMinWidth(200);
+		buyTickets.setMaxWidth(200);
+		eventFeature.setBottom(buyTickets);
+		
+		HBox center = new HBox(30, image, eventInformation);
+		center.setAlignment(Pos.CENTER);
+		
+		eventFeature.setCenter(center);;
+		eventFeature.setAlignment(eventFeature.getTop(), Pos.CENTER);
+		eventFeature.setAlignment(eventFeature.getCenter(), Pos.CENTER);
+		eventFeature.setAlignment(eventFeature.getBottom(), Pos.CENTER);
+
+		eventFeature.setMinHeight(600);
+		eventFeature.setMaxHeight(600);
+	}
+
+	private void initializeEventsList() {
 		eventsList = new LinkedList<>();
 		
 		Event event1 = new Event();
@@ -55,24 +121,46 @@ public class Events {
 		event2.time = "7:00 PM";
 		event2.location = "Bookends Store";
 		event2.description = "Join us as founding member and drummer of Talking Heads band "
-				+ "and member of The Rock and Roll Hall of Fame, Chris Frants, will sign his new book: "
-				+ "Remain in Love.";
+				+ "and member of The Rock and Roll Hall of Fame, Chris Frants, will talk about "
+				+ "his new book: Remain in Love."
+				+ "\n\nChris will be signing all books and taking pictures with those who attend!";
 		eventsList.add(event2);
+		
+		Event event3 = new Event();
+		event3.title = "Kristen Doute Tells All";
+		event3.author = "Kristen Doute";
+		event3.book = "He's Making You Crazy";
+		event3.image = new Image("event3.jpg");
+		event3.date = "Sunday, May 31st";
+		event3.time = "1:00 PM";
+		event3.location = "Bookends Store";
+		event3.description = "Join us as star of Vanderpump Rules, Kristen Doute, shares all about "
+				+ "her new book, He's Making You Crazy: How to Get the Guy, Get Even, and Get Over It."
+				+ "\n\nAs always, all books will be signed and you'll have a chance to meet the author.";
+		eventsList.add(event3);
 	}
 
 	private void createEventsPage() {
-    	events = new StackPane(createEventList());
+		HBox eventsPageHBox = new HBox(80, createEventList(), eventFeature);
+		eventsPageHBox.getStyleClass().add("main");
+    	events = new StackPane(eventsPageHBox);
+    	events.getStyleClass().add("main");
     	events.getStylesheets().add("events.css");
+    	selectEvent(eventPreviews.get(0));
+    	events.setAlignment(Pos.CENTER);
     }
     
-    public StackPane getEventsPage(){
+
+	public StackPane getEventsPage(){
         return events;
     }
     
     private VBox createEventList() { 	
     	eventPreviews = new LinkedList<>();
-    	VBox eventListVBox = new VBox(10);
-    	eventListVBox.setPadding(new Insets(20));
+    	Text upcomingEventsText = new Text("Upcoming Events");
+    	upcomingEventsText.getStyleClass().add("header");
+    	VBox eventListVBox = new VBox(10, upcomingEventsText);
+    	//eventListVBox.setPadding(new Insets(20));
     	
     	for(Event event : eventsList) {
     		StackPane preview = createEventPreview(event);
@@ -89,6 +177,7 @@ public class Events {
         	deselectEvent(selectedEvent);
 			selectEvent(preview);
 			selectedEvent = eventPreviews.indexOf(preview);
+			updateEventFeature(eventsList.get(selectedEvent));
 		});
 	}
 
